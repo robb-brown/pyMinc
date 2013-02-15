a3e1e71e5a8ad63c2b023c0bfdb8f4a924280322
Robert Brown <robert.brown@mcgill.ca>
2013-02-15 10:48:31 -0500

    VolumeIO Wrapping

--- build/lib.macosx-10.6-intel-2.7/pyMinc/examples.py
+++
@@ -1,9 +0,0 @@
-from mincFile import *
-from pylab import *
-
-fname = '/Volumes/Data/Main/bmt_002-OTT-1_chafe_1234_baseline1_MTR.mnc.gz'
-
-minc = mincFile(fname)
-data = minc.getNumpyArray()
-print max(ravel(data))
-imshow(data[30]); colorbar()
