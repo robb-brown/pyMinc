c8a85392ade5528b2ffc64be2eb0f764fb874075
Robert Brown <robert.brown@mcgill.ca>
2016-06-14 14:39:04 -0400

    bugfix in linearResample

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 
 
 sourcefiles = ['pyMinc.c','pyMinctracc.c']
-libraries = ['minc2','volume_io2','netcdf','hdf5','curl','minctracc']
+libraries = ['minc2','netcdf','hdf5','curl','minctracc'] 	# 'volume_io2' - turned into minc2 in later versions
 include_dirs = ['.','pyMinctracc','/usr/local/include','/usr/local/minc-toolkit/include']
 library_dirs = ['.','pyMinctracc','/usr/lib','/usr/local/lib','/usr/local/minc-toolkit/lib']
 
